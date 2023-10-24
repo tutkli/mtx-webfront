0737fe89121109f13b9107621bc664c59977c8f1
Clara Castillo <claracastillochale@gmail.com>
2023-10-24 12:23:30 +0200

    test: add jest

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AppComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create the app', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it(`should have as title 'mtx-webfront'`, () => {
+        expect(component.title).toEqual('mtx-webfront');
+    });
+
+    it('should render the title', () => {
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('.content')?.textContent).toContain('mtx-webfront');
+    });
+});
