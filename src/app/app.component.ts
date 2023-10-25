3fb329bf1202112f64dd7e4fd8c4a060c5da30c4
Clara Castillo <claracastillochale@gmail.com>
2023-10-25 17:16:24 +0200

    feat: add angular material (sigh...)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ToolbarComponent } from '@shared/ui/toolbar/toolbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'mtx-root',
   standalone: true,
-  imports: [ToolbarComponent],
+  imports: [ToolbarComponent, MatSidenavModule],
   template: `<div class="h-screen w-screen">
     <mtx-toolbar color="primary">
       <span>Mejora tu ciudad</span>
     </mtx-toolbar>
+    <mat-drawer-container autosize class="box-border h-[calc(100%-56px)]">
+      <mat-drawer mode="side" [opened]="open()">Drawer content</mat-drawer>
+      <mat-drawer-content class="box-border bg-background p-4">
+        <button class="rounded-lg bg-primary p-2" (click)="open.set(!open())">
+          Toggle
+        </button>
+      </mat-drawer-content>
+    </mat-drawer-container>
   </div>`,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent {
+  open = signal(true);
+}
