617512c15254c08f4d651b9880d4f2f2fd914174
Clara Castillo <claracastillochale@gmail.com>
2023-10-25 21:57:53 +0200

    feat: add ui button

---
+++ src/app/shared/ui/button/button.directive.ts
@@ -0,0 +1,62 @@
+import { Directive, Input, OnChanges, signal } from '@angular/core';
+import { VariantProps } from 'cva';
+import { hostBinding } from 'ngxtension/host-binding';
+import { cva } from '@utils/cva';
+
+const buttonVariants = cva({
+  base: 'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  variants: {
+    type: {
+      default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+      destructive:
+        'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+      outline:
+        'border border-input hover:bg-accent hover:text-accent-foreground',
+      secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+      ghost: 'hover:bg-accent hover:text-accent-foreground',
+      link: 'underline-offset-4 hover:underline text-primary',
+    },
+    size: {
+      default: 'h-10 py-2 px-4',
+      sm: 'h-9 px-3 rounded-md',
+      lg: 'h-11 px-8 rounded-md',
+      icon: 'h-10 w-10',
+    },
+  },
+  defaultVariants: {
+    type: 'default',
+    size: 'default',
+  },
+});
+type ButtonVariants = VariantProps<typeof buttonVariants>;
+
+@Directive({
+  selector: '[mtxButton]',
+  standalone: true,
+})
+export class ButtonDirective implements OnChanges {
+  @Input() type: ButtonVariants['type'] = 'default';
+  @Input() size: ButtonVariants['size'] = 'default';
+  @Input() class = '';
+
+  private hostClass = hostBinding(
+    'attr.class',
+    signal(
+      buttonVariants({
+        type: this.type,
+        size: this.size,
+        className: this.class,
+      })
+    )
+  );
+
+  ngOnChanges() {
+    this.hostClass.set(
+      buttonVariants({
+        type: this.type,
+        size: this.size,
+        className: this.class,
+      })
+    );
+  }
+}
