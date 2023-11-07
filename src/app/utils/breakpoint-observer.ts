fbd161984d6a111917827ecb32bab2de38c66d71
Clara Castillo <claracastillochale@gmail.com>
2023-11-07 12:05:32 +0100

    feat: add breakpoint consts

--- src/app/utils/breakpoint-observer.ts
+++ src/app/utils/breakpoint-observer.ts
@@ -1,3 +1,5 @@
 import { breakpointsTailwind, observeBreakpoints } from 'ngx-breakpoint-observer';
 
 export const breakpointObserver = observeBreakpoints(breakpointsTailwind);
+
+export const xsBreakpoint = breakpointObserver.smallerOrEqual('sm');
