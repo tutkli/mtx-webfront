4a30615b1d82464efe5904cf0671d777465e8bdc
Clara Castillo <claracastillochale@gmail.com>
2023-11-14 14:59:17 +0100

    chore: remove @ngneat/hotToast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHotToastConfig } from '@ngneat/hot-toast';
 import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@ngneat/transloco';
 import { JurisdictionService } from '@core/services/jurisdiction/jurisdiction.service';
@@ -15,9 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
-    provideHotToastConfig({
-      position: 'bottom-right',
-    }),
     provideTransloco({
       config: {
         availableLangs: ['es', 'en'],
